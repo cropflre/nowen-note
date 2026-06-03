157e2e82a20066f8efca66579146c8ef0b4e2caf
cropflre <767308865@qq.com>
2026-06-03 14:22:13 +0800

    chore: 提交剩余改动

---
+++ fix_sidebar.ps1
@@ -0,0 +1,8 @@
+﻿$lines = [System.IO.File]::ReadAllLines('C:\UGit\nowen-note\frontend\src\components\Sidebar.tsx')
+$newLines = [System.Collections.ArrayList]::new()
+for ($i = 0; $i -lt $lines.Length; $i++) {
+    if ($i -eq 642) { continue }
+    [void]$newLines.Add($lines[$i])
+}
+[System.IO.File]::WriteAllLines('C:\UGit\nowen-note\frontend\src\components\Sidebar.tsx', $newLines)
+Write-Host "Done. Removed line 643 (0-indexed 642). New line count: $($newLines.Count)"
