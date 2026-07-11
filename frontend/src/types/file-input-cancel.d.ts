41485f6650d5bc7b0a8b098ecec98060d5cd8d13
Drug <cropflre2333@gmail.com>
2026-07-11 19:21:11 +0800

    chore(media): rely on DOM cancel event typing (#214)

--- frontend/src/types/file-input-cancel.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-declare global {
-  interface HTMLInputElement {
-    /** Supported by modern mobile file pickers; absent from older TypeScript DOM declarations. */
-    oncancel: ((this: HTMLInputElement, event: Event) => unknown) | null;
-  }
-}
