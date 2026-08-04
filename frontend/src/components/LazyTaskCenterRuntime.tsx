c3eb7c4265821ee0fc2d750fe45ebfdf6c46067a
Drug <cropflre2333@gmail.com>
2026-08-04 21:43:52 +0800

    fix(frontend): type lazy no-prop centers

--- frontend/src/components/LazyTaskCenterRuntime.tsx
+++ frontend/src/components/LazyTaskCenterRuntime.tsx
@@ -2,12 +2,11 @@ import React, { Suspense } from "react";
 import LazyWorkspaceFallback from "./LazyWorkspaceFallback";
 
 const LazyTaskCenter = React.lazy(() => import("./TaskCenter"));
-type Props = React.ComponentProps<(typeof import("./TaskCenter"))["default"]>;
 
-export default function LazyTaskCenterRuntime(props: Props) {
+export default function LazyTaskCenterRuntime() {
   return (
     <Suspense fallback={<LazyWorkspaceFallback label="正在加载任务中心…" />}>
-      <LazyTaskCenter {...props} />
+      <LazyTaskCenter />
     </Suspense>
   );
 }
